9a464b978d86974c561ce55caafce6d8bc105724
Changmin Lee <chlee@Changminui-MacBookAir.local>
2023-09-15 13:51:28 +0900

    ADD: solved B2 recursion problem

---
+++ Baekjoon/25501.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string.h>
+#include <string>
+
+int Cnt;
+int recursion(const char *s, int l, int r){
+    Cnt++;
+    if(l >= r) return 1;
+    else if(s[l] != s[r]) return 0;
+    else return recursion(s, l+1, r-1);
+}
+
+int isPalindrome(const char *s){
+    return recursion(s, 0, strlen(s)-1);
+}
+
+int main(){
+    int T;
+    std::string S;
+
+    std::cin >> T;
+    while (T--) {
+        Cnt = 0;
+        std::cin >> S;
+        std::cout << isPalindrome(S.c_str()) << " " << Cnt << std::endl;
+    }
+    return 0;
+}
