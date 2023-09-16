6011711a47a48b664809a6a0dadf05124029be47
Moongss <mininori@naver.com>
2023-09-16 15:56:18 +0900

    ADD: solved S2 parametric search problem

--- Baekjoon/6236.cpp
+++ Baekjoon/6236.cpp
@@ -3,28 +3,43 @@
 int main() {
     int N, M;
     int arr[100001] = {0, };
-    int pre[100001] = {0, };
 
+    int min = 1000000;
+    int max = -1;
     std::cin >> N >> M;
-    for (int i = 1; i <= N; i++) {
+    for (int i = 0; i < N; i++) {
         std::cin >> arr[i];
-        pre[i] = pre[i - 1] + arr[i];
+
+        max = (max < arr[i]) ? arr[i] : max;
+        min = (min > arr[i]) ? arr[i] : min;
     }
 
-    int min = 100000 * 10000;
-    int cnt = N - M + 1;
-    for (int i = 0; i < N - cnt; i++) {
-        int l = i;
-        int r = i + cnt;
-        int current = pre[r] - pre[l];
+    long lo = 1;
+    long hi = 10000 * 100000;
+    while (lo + 1 < hi) {
 
-        std::cout << "current : " << current << std::endl;
-        if (current < min) min = current;
+        long mid = (lo + hi) / 2;
+        int cnt = 0;
+        int money = 0;
+        for (int i = 0; i < N; i++) {
+            if (mid < arr[i]) {
+                cnt = M + 1;
+                break;
+            }
+            while (money < arr[i]) {
+                money = mid;
+                cnt++;
+            }
+            money -= arr[i];
+        }
+        
+        // std::cout << "mid : " << mid << std::endl;
+        // std::cout << "cnt : " << cnt << std::endl;
+        //M = 1 2 3 4 5 6 7 8 9
+        //M = f f f f t t t t t
+        if (cnt <= M) hi = mid;
+        else lo = mid;
     }
-    
-    if (min == 100000 * 10000)
-        min = 0;
-
-    std::cout << min << std::endl;
+    std::cout << hi << std::endl;
     return 0;
 }
