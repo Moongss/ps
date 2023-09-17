2c0ac903b77e29b022bbf9912da9d95c45cbb047
chlee <chlee@student.42seoul.kr>
2023-09-17 19:01:17 +0900

    ADD: solved B5 implementation problem

---
+++ Baekjoon/1809.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    std::cout << "  ___  ___  ___" << std::endl;
+    std::cout << "  | |__| |__| |" << std::endl;
+    std::cout << "  |           |" << std::endl;
+    std::cout << "   \\_________/" << std::endl;
+    std::cout << "    \\_______/" << std::endl;
+    std::cout << "     |     |" << std::endl;
+    std::cout << "     |     |" << std::endl;
+    std::cout << "     |     |" << std::endl;
+    std::cout << "     |     |" << std::endl;
+    std::cout << "     |_____|" << std::endl;
+    std::cout << "  __/       \\__" << std::endl;
+    std::cout << " /             \\" << std::endl;
+    std::cout << "/_______________\\"  << std::endl;
+    return 0;
+}
