ff0b87550566590600a9cbf9cbfa6dbb486a308a
chlee <chlee@student.42seoul.kr>
2023-09-18 14:44:05 +0900

    ADD: solved S2 greedy problem

---
+++ Baekjoon/30022.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+
+int main() {
+    std::cin.tie(0);
+    std::cout.tie(0);
+    std::ios_base::sync_with_stdio(false);
+
+    int N, A, B;
+    int P[100000];
+    int Q[100000];
+    
+    std::cin >> N >> A >> B;
+    long long result = 0;
+
+    std::priority_queue<std::pair<int, std::pair<int, int> > > pq;
+
+    for (int i = 0; i < N; i++) {
+        std::cin >> P[i] >> Q[i];
+        
+        pq.push(std::make_pair(Q[i] - P[i], std::make_pair(P[i], Q[i])));
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (A) {
+            result += pq.top().second.first;
+            A--;
+        } else {
+            result += pq.top().second.second;
+        }
+        pq.pop();
+    }
+
+    std::cout << result << std::endl;
+    return 0;
+}
