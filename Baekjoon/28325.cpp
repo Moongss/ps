c486192797f760a9954739ed7efd605fdc40d6da
Moongss <mininori@naver.com>
2023-09-28 19:49:07 +0900

    ADD: solved G3 greedy/Ad-hoc problem

--- Baekjoon/28325.cpp
+++ Baekjoon/28325.cpp
@@ -1,37 +1,39 @@
 #include <iostream>
-#include <queue>
 
 int N;
-std::priority_queue<std::pair<long long, int> > pq;
 long long cnt[250000] = {0, };
-bool valid[250000] = {true, };
 
 int main() {
+    long long result = 0;
+    int maxIdx = 0;
+    long long max = -1;
     std::cin >> N;
     for (int i = 0; i < N; i++) {
-        long long tmp;
-        std::cin >> tmp;
-
-        valid[i] = true;
-        pq.push(std::make_pair(tmp + 1, i));
+        std::cin >> cnt[i];
+        result += cnt[i];
+        if (cnt[i] > max) {
+            max = cnt[i];
+            maxIdx = i;
+        }
     }
 
-    long long result = 0;
-    while (!pq.empty()) {
-        std::pair<long long, int> elem = pq.top();
-        pq.pop();
-
-        long long val = elem.first;
-        int idx = elem.second;
-
-        int prev = (idx + N - 1) % N;
-        int next = (idx + 1) % N;
-        if (valid[prev] && valid[next] && 
-            (cnt[prev] + cnt[next] < val ||
-            (cnt[prev] + cnt[next] == val && idx % 2 == 1))) {
-            result += val;
-            valid[idx] = false;
-            cnt[prev] = cnt[next] = 0; //never choose.
+    if (result == 0) {
+        std::cout << N / 2 << std::endl;
+        return 0;
+    } else {
+        for (int i = maxIdx; i < N + maxIdx; i++) {
+            if (cnt[i % N]) {
+                continue;
+            }
+            else {
+                int tmp = 0;
+                while (!cnt[i % N]) {
+                    tmp++;
+                    i++;
+                }
+                result += (tmp + 1) / 2;
+                i--;
+            }
         }
     }
 
