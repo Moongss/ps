35bec8650d84d21581bc9c86855c6b90e7cf330a
Moongss <mininori@naver.com>
2023-10-09 14:15:10 +0900

    ADD: solved B3 implementation problem

---
+++ Baekjoon/1247.py
@@ -0,0 +1,11 @@
+for i in range(3):
+    N = int(input())
+    sum = 0
+    for j in range(N):
+        sum += int(input())
+    if sum > 0:
+        print('+')
+    elif sum < 0:
+        print('-')
+    else:
+        print('0')
