fb5ce62aa9e8533e802d06ded2c8e5035e6bb888
chlee <chlee@student.42seoul.kr>
2023-10-10 19:04:14 +0900

    ADD: solved D5 inclusion-exclusion principle problem

--- Baekjoon/1557.cpp
+++ Baekjoon/1557.cpp
@@ -5,19 +5,37 @@ using ll = long long;
 
 ll K;
 
-int prime[45000] = {0, };
-std::vector<std::pair<int, ll> > v;
+int prime[40558] = {0, }; // 45000, tight: 40558 (K = 1e9 -> 1644934081, sqrt(K) = 40558.xx)
+std::vector<int> v;
 
-void primeSet() {
-    for (int i = 2; i <= 45000; i++) {
+void primeSet() { // n log log n
+    for (int i = 2; i <= 40558; i++) {
         prime[i] = i;
     }
     
-    for (int i = 2; i <= 45000; i++) {
+    for (int i = 2; i <= 40558; i++) {
         if (prime[i] == 0) continue;
-        for (int j = i + i; j <= 45000; j += i) {
+        for (int j = i + i; j <= 40558; j += i) {
             prime[j] = 0;
         }
+    }
+}
+
+ll cnt = 0;
+void solve(ll N, ll idx, ll cur, ll num) {
+    if (num > 6 || N < cur * v[idx])
+        return;
+
+    while (idx < v.size()) {
+        if (N < cur * v[idx])
+            break;
+        if (num % 2 == 1) {
+            cnt += N / (cur * v[idx]);
+        } else {
+            cnt -= N / (cur * v[idx]);
+        }
+        idx++;
+        solve(N, idx, cur * v[idx - 1], num + 1);
     }
 }
 
@@ -27,61 +45,19 @@ int main() {
     std::cin >> K;
 
     ll lo = 0;
-    ll hi = 2e9;
+    ll hi = 1644934081 + 1; // K = 1e9, ans = 1644934081
 
     while (lo + 1 < hi) {
         ll mid = (lo + hi) / 2;
-
-        ll cnt = 0;
         v.clear();
         for (ll i = 2; i * i <= mid; i++) {
             if (prime[i] == 0) continue;
-            cnt += mid / (i * i);
-            v.push_back(std::make_pair(i, mid / (i * i)));
+            v.push_back(i * i);
         }
 
-        std::cout << "====================" << std::endl;
-        std::cout << "mid : " << mid << ", cnt : " << cnt << std::endl;
-        // for (int i = 0; i < v.size(); i++) {
-        //     std::cout << "v[i] : " << v[i].first << ", " << v[i].second << std::endl;
-        // }
-        
-        ll tmp = 0;
-        for (int i = 0; i < v.size() - 1; i++) {
-            for (int j = i + 1; j < v.size(); j++) {
-                if (v[j].second < (v[i].first * v[i].first))
-                    continue;
-                else {
-                    // std::cout << "i :  "<< i << std::endl;
-                    // std::cout << "j :  "<< j << std::endl;
-                    ll repeat = v[j].second / (v[i].first * v[i].first);
-                    tmp += repeat;
-                    for (int k = i + 1; k < j; k++) {
-                        if (repeat < (v[k].first * v[k].first)) {
-                            break;
-                        } else {
-                            std::cout << "v[k] : " << v[k].first << ", " << v[k].second << std::endl;
-                            std::cout << "repeat : " << repeat << std::endl;  
-                            std::cout << "minus!\n";
-                            tmp -= repeat / (v[k].first * v[k].first);
-                            // repeat %= (v[k].first * v[k].first);
-                            // break;
-                        }
-                    }
-                    std::cout << std::endl;
-                    // if minus, not ...
-                    // v[j].second -= v[j].second / (v[i].first * v[i].first);
-                }
-            }
-        }
+        cnt = 0;
+        solve(mid, 0, 1, 1);
 
-        cnt -= tmp;
-        // for (int i = 0; i < v.size(); i++) {
-        //     std::cout << "v[i] : " << v[i].first << ", " << v[i].second << std::endl;
-        // }
-
-
-        std::cout << "mid : " << mid << ", cnt : " << cnt << std::endl;
         if (mid - cnt >= K) {
             hi = mid;
         } else {
