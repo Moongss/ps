a15e8df793c30462e9bd0a0640ca41394bb635d2
chlee <chlee@student.42seoul.kr>
2023-10-28 16:39:25 +0900

    MODIFY: reduce arr size, time

--- Baekjoon/25682.cpp
+++ Baekjoon/25682.cpp
@@ -9,7 +9,7 @@ using namespace std;
 using ll = long long;
 
 int N, M, K;
-int pre[2][2001][2001];
+int pre[2001][2001];
 
 int main() {
     fastio;
@@ -17,30 +17,17 @@ int main() {
     for (int i = 1; i <= N; i++) {
         for (int j = 1; j <= M; j++) {
             char ch; cin >> ch;
-            if (ch == 'B' && (abs(i - j) % 2 == 0))
-                pre[BLACK][i][j] = 1;
-            if (ch == 'W' && (abs(i - j) % 2 == 1))
-                pre[WHITE][i][j] = 1;
-        }
-        for (int j = 1; j <= M; j++) {
-            pre[BLACK][i][j] += pre[BLACK][i][j - 1];
-            pre[WHITE][i][j] += pre[WHITE][i][j - 1];
-        }
-    }
-
-    for (int j = 1; j <= M; j++) {
-        for (int i = 1; i <= N; i++) {
-            pre[BLACK][i][j] += pre[BLACK][i - 1][j];
-            pre[WHITE][i][j] += pre[WHITE][i - 1][j];
+            pre[i][j] = pre[i - 1][j] + pre[i][j - 1] - pre[i - 1][j - 1];
+            if (ch == 'B' && (i + j) % 2 == BLACK || ch == 'W' && (i + j) % 2 == WHITE)
+                pre[i][j]++;
         }
     }
 
     int result = 42424242;
     for (int i = K; i <= N; i++) {
         for (int j = K; j <= M; j++) {
-            int blackCnt = pre[BLACK][i][j] - pre[BLACK][i][j - K] - pre[BLACK][i - K][j] + pre[BLACK][i - K][j - K];
-            int whiteCnt = pre[WHITE][i][j] - pre[WHITE][i][j - K] - pre[WHITE][i - K][j] + pre[WHITE][i - K][j - K];
-            result = min(result, min(K * K - (blackCnt + whiteCnt), blackCnt + whiteCnt));
+            int sum = pre[i][j] - pre[i][j - K] - pre[i - K][j] + pre[i - K][j - K];
+            result = min(result, min(K * K - sum, sum));
         }
     }
     cout << result << endl;
