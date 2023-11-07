2d21f7e7e5bdb68c385066b5eea5dc03af99c002
chlee <chlee@student.42seoul.kr>
2023-11-07 16:40:45 +0900

    ADD: solved S5 game theory problem

---
+++ Baekjoon/9655.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define fastio cin.tie(0)->sync_with_stdio(0)
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int main() {
+    fastio;
+    int N; cin >> N;
+    cout << (N % 2 ? "SK" : "CY") << endl;
+    return 0;
+}
