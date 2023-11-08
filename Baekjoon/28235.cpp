28e472c60186779a321ba460612ea3e9f0da739a
chlee <chlee@student.42seoul.kr>
2023-11-08 20:28:41 +0900

    ADD: solved boj B5 implementation problem

---
+++ Baekjoon/28235.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define fastio cin.tie(0)->sync_with_stdio(0)
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int main() {
+    fastio;
+    string str; cin >> str;
+    if (str == "SONGDO") cout << "HIGHSCHOOL" << endl;
+    if (str == "CODE") cout << "MASTER" << endl;
+    if (str == "2023") cout << "0611" << endl;
+    if (str == "ALGORITHM") cout << "CONTEST" << endl;
+    return 0;
+}
