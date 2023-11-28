4d094a882b83bf3216b593f7fa145dd28b00bc9d
chlee <chlee@student.42seoul.kr>
2023-11-29 02:02:17 +0900

    ADD: solved B5 implementation problem

---
+++ Baekjoon/7891.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define fastio cin.tie(0)->sync_with_stdio(0)
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int main() {
+    fastio;
+    int t; cin >> t;
+    while (t--) {
+        ll a, b; cin >> a >> b;
+        cout << a + b << endl;
+    }
+    return 0;
+}
