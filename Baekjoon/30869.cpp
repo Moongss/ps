6b99654fea249f9798b14b109dc3fa7d07448978
chlee <chlee@student.42seoul.kr>
2023-12-05 00:40:05 +0900

    MODIFY: edge case

--- Baekjoon/30869.cpp
+++ Baekjoon/30869.cpp
@@ -1,3 +1,7 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
 #include <bits/stdc++.h>
 #define endl "\n"
 #define fastio cin.tie(0)->sync_with_stdio(0)
@@ -6,13 +10,15 @@ using namespace std;
 using ll = long long;
 using ld = long double;
 
-int dp[501];
+int dp[501][501];
 vector<pair<int, pair<short, short>>> adj[501];
 priority_queue<pair<int, pair<short, short>>, 
-            vector<pair<int, pair<short, short>>>> pq;
+            vector<pair<int, pair<short, short>>>,
+            greater<pair<int, pair<short, short>>>> pq;
 
 int main() {
     fastio;
+
     int N, M, K;
     cin >> N >> M >> K;
     for (int i = 0; i < M; i++) {
@@ -21,35 +27,44 @@ int main() {
         adj[s].push_back({e, {t, g}});
     }
 
-    memset(dp, 0x3f3f3f3f, sizeof(dp));
+    for (int i = 0; i <= N; i++)
+        for (int j = 0; j <= K; j++)
+            dp[i][j] = 0x3f3f3f3f;
+
     pq.push({0, {1, 0}});
-    dp[1] = 0;
+    dp[1][0] = 0;
     while (!pq.empty()) {
         int curTime = pq.top().first;
         int cur = pq.top().second.first;
         int curK = pq.top().second.second;
         pq.pop();
 
-        if (cur == N) continue;
-        if (curTime > dp[cur]) continue;
+        if (curTime > dp[cur][curK]) continue;
         for (auto &edge : adj[cur]) {
             int next = edge.first;
             int t = edge.second.first;
             int g = edge.second.second;
 
-            int waitTime = ((dp[cur] + g - 1) / g) * g;
-            if (dp[next] > waitTime + t) {
-                dp[next] = waitTime + t;
-                pq.push({dp[next], {next, curK}});
+            // 바로 탑승
+            if (curK < K && dp[next][curK + 1] > curTime + t && dp[next][curK] > curTime + t) {
+                dp[next][curK + 1] = curTime + t;
+                pq.push({dp[next][curK + 1], {next, curK + 1}});
             }
 
-            if (curK < K && dp[next] > dp[cur] + t) { //if waitTime == 0, don't run this code
-                dp[next] = dp[cur] + t;
-                pq.push({dp[next], {next, curK + 1}});
+            // 기다리고 탑승
+            int waitTime = ((curTime + g - 1) / g) * g;
+            if (dp[next][curK] > waitTime + t && (curK == 0 || dp[next][curK - 1] > waitTime + t)) {
+                dp[next][curK] = waitTime + t;
+                pq.push({dp[next][curK], {next, curK}});
             }
         }
     }
 
-    cout << (dp[N] == 0x3f3f3f3f ? -1 : dp[N]) << endl;
+    int ans = 0x3f3f3f3f;
+    for (int i = 0; i <= K; i++) {
+        ans = min(ans, dp[N][i]);
+    }
+
+    cout << (ans == 0x3f3f3f3f ? -1 : ans) << endl;
     return 0;
 }
