a50423b62b25f5e5cefd18106f0dc26f720baca1
chlee <chlee@student.42seoul.kr>
2023-12-12 15:45:23 +0900

    ADD: solved P3 rotating-calipers problem

---
+++ Baekjoon/4225.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define fastio cin.tie(0)->sync_with_stdio(0)
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+struct Point {
+    ll x, y;
+    Point() {};
+    Point(ll x, ll y) : x(x), y(y) {};
+    bool operator< (const Point& p) const {
+        if (y == p.y) return x < p.x;
+        return y < p.y;
+    }
+};
+
+int N;
+vector<Point> p;
+
+ll ccw(Point p1, Point p2, Point p3) {
+    ll ret = (p1.x * p2.y + p2.x * p3.y + p3.x * p1.y) - (p1.y * p2.x + p2.y * p3.x + p3.y * p1.x);
+    return (ret > 0) - (ret < 0);
+}
+
+ll cross(Point &p1, Point &p2) {
+    return p1.x * p2.y - p1.y * p2.x;
+}
+
+ll dist(Point &p1, Point &p2) {
+    return (p1.x - p2.x) * (p1.x - p2.x) +
+           (p1.y - p2.y) * (p1.y - p2.y);
+}
+
+bool cmp(Point &p1, Point &p2) {
+    ll ret = ccw(p[0], p1, p2);
+    if (ret == 0) return dist(p[0], p1) < dist(p[0], p2);
+    return ret > 0;
+}
+
+int main() {
+    fastio;
+    int cnt = 0;
+
+    while (true) {
+        cin >> N;
+        if (N == 0) break;
+
+        cnt++;
+        p.clear();
+        for (int i = 0; i < N; i++) {
+            ll x, y; cin >> x >> y;
+            p.push_back(Point(x, y));
+        }
+
+        sort(p.begin(), p.end());
+        sort(p.begin() + 1, p.end(), cmp);
+
+        vector<int> cv;
+        cv.push_back(0);
+        cv.push_back(1);
+        int next = 2;
+
+        while (next < N) {
+            while (cv.size() >= 2) {
+                int second = cv.back(); cv.pop_back();
+                int first = cv.back();
+                if (ccw(p[first], p[second], p[next]) > 0) {
+                    cv.push_back(second);
+                    break;
+                }
+            }
+            cv.push_back(next++);
+        }
+
+        ld ans = 424242424242;
+        Point originVector = {0, 0};
+        for (int i = 0, j = 1; i < cv.size(); i++) {
+            while (i != j) {
+                int next_i = (i + 1) % cv.size();
+                int next_j = (j + 1) % cv.size();
+
+                Point lVector = Point(p[cv[next_i]].x - p[cv[i]].x, p[cv[next_i]].y - p[cv[i]].y);
+                Point rVector = Point(p[cv[next_j]].x - p[cv[j]].x, p[cv[next_j]].y - p[cv[j]].y);
+                if (ccw(originVector, lVector, rVector) > 0) j = next_j;
+                else break;
+            }
+            //외적 이용해서 구하기
+            Point u = Point(p[cv[(i + 1) % cv.size()]].x - p[cv[i]].x,
+                            p[cv[(i + 1) % cv.size()]].y - p[cv[i]].y);
+            Point v = Point(p[cv[j]].x - p[cv[i]].x, p[cv[j]].y - p[cv[i]].y);
+
+            ld d = cross(u, v) / sqrt(dist(u, originVector) * 1.0);
+            ans = min(ans, d);
+        }
+        cout << "Case " << cnt << ": ";
+        cout << fixed << setprecision(2) << ceil(ans * 100) / 100 << endl;
+    }
+    return 0;
+}
