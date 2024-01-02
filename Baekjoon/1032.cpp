1bad748a51272e790e46843bc09f56e6e26c6ee2
chlee <chlee@student.42seoul.kr>
2024-01-02 14:03:33 +0900

    ADD: solved B5 implementation problem

---
+++ Baekjoon/1032.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define fastio cin.tie(0)->sync_with_stdio(0)
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+int main() {
+    fastio;
+    
+    int N; cin >> N;
+    string result; cin >> result;
+
+    for (int i = 0; i < N - 1; i++) {
+        string s; cin >> s;
+        for (int j = 0; j < s.size(); j++) {
+            if (result[j] != s[j]) result[j] = '?';
+        }
+    }
+    vector<string> v(N);
+
+    return 0;
+}
