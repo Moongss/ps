decf6a1d46dcc1b45cbda280ea942a193740e385
Moongss <mininori@naver.com>
2024-01-07 22:44:12 +0900

    WIP

--- Baekjoon/17470.cpp
+++ Baekjoon/17470.cpp
@@ -3,8 +3,6 @@
 #define endl '\n';
 using namespace std;
 
-using pii = pair<int, int>;
-
 int N, M, R;
 int max;
 int inputArr[100][100];
@@ -19,9 +17,9 @@ int rotation[100][100] = { // 4x4
 
 int rotateCnt = 0;
 int operCnt[7];
+int tmp2[100][100];
 
 void rotateArray2(int flag) {
-    int tmp[100][100];
     if (flag == 2) {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M / 2; j++) {
@@ -43,7 +41,7 @@ void rotateArray2(int flag) {
     } else if (flag == 3) {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                tmp[j][N - i - 1] = inputArr[i][j]; // 0 위치 바꾸기
+                tmp2[j][N - i - 1] = inputArr[i][j]; // 0 위치 바꾸기
             }
         }
         int temp = N;
@@ -52,7 +50,7 @@ void rotateArray2(int flag) {
     } else if (flag == 4) {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                tmp[M - j - 1][i] = inputArr[i][j];
+                tmp2[M - j - 1][i] = inputArr[i][j];
             }
         }
         int temp = N;
@@ -63,13 +61,13 @@ void rotateArray2(int flag) {
             for (int i = 0; i < N / 2; i++) {
                 for (int j = 0; j < M / 2; j++) {
                     if (k == 0)
-                        tmp[i][j] = inputArr[N / 2 + i][j];
+                        tmp2[i][j] = inputArr[N / 2 + i][j];
                     if (k == 1)
-                        tmp[i][M / 2 + j] = inputArr[i][j];
+                        tmp2[i][M / 2 + j] = inputArr[i][j];
                     if (k == 2)
-                        tmp[N / 2 + i][M / 2 + j] = inputArr[i][M / 2 + j];
+                        tmp2[N / 2 + i][M / 2 + j] = inputArr[i][M / 2 + j];
                     if (k == 3)
-                        tmp[N / 2 + i][j] = inputArr[N / 2 + i][M / 2 + j];
+                        tmp2[N / 2 + i][j] = inputArr[N / 2 + i][M / 2 + j];
                 }
             }
         }
@@ -78,26 +76,27 @@ void rotateArray2(int flag) {
             for (int i = 0; i < N / 2; i++) {
                 for (int j = 0; j < M / 2; j++) {
                     if (k == 0)
-                        tmp[i][j] = inputArr[i][M / 2 + j];
+                        tmp2[i][j] = inputArr[i][M / 2 + j];
                     if (k == 1)
-                        tmp[i][M / 2 + j] = inputArr[N / 2 + i][M / 2 + j];
+                        tmp2[i][M / 2 + j] = inputArr[N / 2 + i][M / 2 + j];
                     if (k == 2)
-                        tmp[N / 2 + i][M / 2 + j] = inputArr[N / 2 + i][j];
+                        tmp2[N / 2 + i][M / 2 + j] = inputArr[N / 2 + i][j];
                     if (k == 3)
-                        tmp[N / 2 + i][j] = inputArr[i][j];
+                        tmp2[N / 2 + i][j] = inputArr[i][j];
                 }
             }
         }
     }
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
-            inputArr[i][j] = tmp[i][j];
+            inputArr[i][j] = tmp2[i][j];
         }
     }
 }
 
 void rotateArray(int N, int M, int flag, int arr[100][100]) {
     int tmp[100][100];
+    memset(tmp, 0, sizeof(tmp));
     if (flag == 2) {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M / 2; j++) {
@@ -165,15 +164,17 @@ void rotateArray(int N, int M, int flag, int arr[100][100]) {
             }
         }
     }
+    cout << "N : " << N << ", M : " << M << endl;
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
             arr[i][j] = tmp[i][j];
+            // cout << arr[i][j] << " ";
         }
+        // cout << endl;
     }
 }
 
 void mappingArray(int r, int c) {
-
     int test[100][100];
     memset(test, 0, sizeof(test));
     // cout << "mapping Array : " << "r : " << r << ", c : " << c << endl;
@@ -183,13 +184,13 @@ void mappingArray(int r, int c) {
         }
     }
 
-    cout << "test" << endl;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            cout << test[i][j] << " ";
-        }
-        cout << endl;
-    }
+    // cout << "test" << endl;
+    // for (int i = 0; i < N; i++) {
+    //     for (int j = 0; j < M; j++) {
+    //         cout << test[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
 
     if (rotation[0][0] % 4 == 0 && rotation[0][1] % 4 == 1) {
         // cout << "DEBUG 1" << endl;
@@ -219,8 +220,10 @@ void mappingArray(int r, int c) {
     }
     if (rotation[1][1] % 4 == 0 && rotation[0][1] % 4 == 1) {
         // cout << "DEBUG 5" << endl;
-        rotateArray(N, M, 3, test);
-        rotateArray(M, N, 1, test);
+        // rotateArray(N, M, 3, test);
+
+        rotateArray(N, M, 4, test);
+        rotateArray(M, N, 1, test);   
     }
     if (rotation[1][1] % 4 == 1 && rotation[0][1] % 4 == 0) {
         // cout << "DEBUG 6" << endl;
@@ -228,7 +231,7 @@ void mappingArray(int r, int c) {
     }
     if (rotation[0][0] % 4 == 0 && rotation[1][0] % 4 == 1) {
         // cout << "DEBUG 7" << endl;
-        rotateArray(N, M, 4, test);
+        rotateArray(N, M, 3, test);
         rotateArray(M, N, 1, test);
     }
     if (rotation[0][0] % 4 == 1 && rotation[1][0] % 4 == 0) {
@@ -242,27 +245,28 @@ void mappingArray(int r, int c) {
                 result[i][j] = test[i][j];
         }
     }
-    cout << "MAPPING RESULT\n";
-    if (rotateCnt) {
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                cout << result[i][j] << " ";
-            }
-            cout << endl;
-        }
-    } else {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                cout << result[i][j] << " ";
-            }
-            cout << endl;
-        }
-    }
+    // cout << "MAPPING RESULT\n";
+    // if (rotateCnt) {
+    //     for (int i = 0; i < M; i++) {
+    //         for (int j = 0; j < N; j++) {
+    //             cout << result[i][j] << " ";
+    //         }
+    //         cout << endl;
+    //     }
+    // } else {
+    //     for (int i = 0; i < N; i++) {
+    //         for (int j = 0; j < M; j++) {
+    //             cout << result[i][j] << " ";
+    //         }
+    //         cout << endl;
+    //     }
+    // }
 }
 
-
+#define fastio cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
 
 int main() {
+    fastio
     std::cin >> N >> M >> R;
 
     for (int i = 0; i < N; i++) {
@@ -272,61 +276,47 @@ int main() {
     }
 
     int operate;
+    int leftCnt = 0;
+    int rightCnt = 0;
     for (int i = 0; i < R; i++) {
         std::cin >> operate;
-        operCnt[operate]++;
+        if (operate == 3) leftCnt++;
+        else if (operate == 4) rightCnt++;
+
+        if (N >= 4 && M >= 4) {
+            rotateArray(4, 4, operate, rotation);
+        }
+        else {
+            rotateArray2(operate);
+        }
     }
 
-    operCnt[1] %= 2;
-    operCnt[2] %= 2;
-    operCnt[3] %= 4;
-    operCnt[4] %= 4;
-    operCnt[5] %= 4;
-    operCnt[6] %= 4;
-
-    rotateCnt = abs(operCnt[3] - operCnt[4]) % 2;
-
+    rotateCnt = abs(leftCnt - rightCnt) % 2;
     if (N >= 4 && M >= 4) {
-        for (int i = 1; i <= 6; i++) {
-            while (operCnt[i]--) {
-                rotateArray(4, 4, i, rotation);
-            }
-        }
-
         mappingArray(0, 0);
         mappingArray(N / 2, 0);
         mappingArray(0, M / 2);
         mappingArray(N / 2, M / 2);
-    } else {
-        for (int i = 1; i <= 6; i++) {
-            while (operCnt[i]--) {
-                rotateArray2(i);
+
+        if (rotateCnt) {
+            for (int i = 0; i < M; i++) {
+                for (int j = 0; j < N; j++) {
+                    cout << result[i][j] << " ";
+                }
+                cout << endl;
             }
-        }
-    }
-
-    
-    // for (int i = 0; i < 4; i++) {
-    //     for (int j = 0; j < 4; j++) {
-    //         std::cout << rotation[i][j] << " ";
-    //     }
-    //     std::cout << endl;
-    // }
-
-
-
-
-    if (rotateCnt) {
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                cout << result[i][j] << " ";
+        } else {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    cout << result[i][j] << " ";
+                }
+                cout << endl;
             }
-            cout << endl;
         }
     } else {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                cout << result[i][j] << " ";
+                cout << inputArr[i][j] << " ";
             }
             cout << endl;
         }
