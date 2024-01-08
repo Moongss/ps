c270ed60e57eb924b3a49fc88c420a89da11911d
chlee <chlee@student.42seoul.kr>
2024-01-08 20:14:31 +0900

    ADD: solved P4 implementation problem

--- Baekjoon/17470.cpp
+++ Baekjoon/17470.cpp
@@ -1,256 +1,237 @@
 #include <bits/stdc++.h>
+#define endl "\n"
+#define fastio cin.tie(0)->sync_with_stdio(0)
 
-#define endl '\n';
 using namespace std;
-
+using ll = long long;
+using ld = long double;
 using pii = pair<int, int>;
+using pll = pair<ll, ll>;
 
 int N, M, R;
-int max;
-int inputArr[100][100];
+int arr[100][100];
+int tmp[100][100];
 int result[100][100];
 
-int rotation[100][100] = { // 4x4
-    {0, 1, 4, 5},
-    {2, 3, 6, 7},
-    {8, 9, 12, 13},
-    {10, 11, 14, 15},
-};
+pii compressMapInfo[4][4];
+pii compressMapTmp[4][4];
 
-int rotateCnt = 0;
-int operCnt[7];
-
-void rotateArray2(int flag) {
-    int tmp[100][100];
-    if (flag == 2) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M / 2; j++) {
-                int tmp = inputArr[i][j];
-                inputArr[i][j] = inputArr[i][M - j - 1];
-                inputArr[i][M - j - 1] = tmp;
-            }
-        }
-        return;
-    } else if (flag == 1) {
-        for (int i = 0; i < N / 2; i++) {
-            for (int j = 0; j < M; j++) {
-                int tmp = inputArr[i][j];
-                inputArr[i][j] = inputArr[N - i - 1][j];
-                inputArr[N - i - 1][j] = tmp;
-            }
-        }
-        return;
-    } else if (flag == 3) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                tmp[j][N - i - 1] = inputArr[i][j]; // 0 위치 바꾸기
-            }
-        }
-        int temp = N;
-        N = M;
-        M = temp;
-    } else if (flag == 4) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                tmp[M - j - 1][i] = inputArr[i][j];
-            }
-        }
-        int temp = N;
-        N = M;
-        M = temp;
-    } else if (flag == 5) {
-        for (int k = 0; k < 4; k++) {
-            for (int i = 0; i < N / 2; i++) {
-                for (int j = 0; j < M / 2; j++) {
-                    if (k == 0)
-                        tmp[i][j] = inputArr[N / 2 + i][j];
-                    if (k == 1)
-                        tmp[i][M / 2 + j] = inputArr[i][j];
-                    if (k == 2)
-                        tmp[N / 2 + i][M / 2 + j] = inputArr[i][M / 2 + j];
-                    if (k == 3)
-                        tmp[N / 2 + i][j] = inputArr[N / 2 + i][M / 2 + j];
-                }
-            }
-        }
-    } else {
-        for (int k = 0; k < 4; k++) {
-            for (int i = 0; i < N / 2; i++) {
-                for (int j = 0; j < M / 2; j++) {
-                    if (k == 0)
-                        tmp[i][j] = inputArr[i][M / 2 + j];
-                    if (k == 1)
-                        tmp[i][M / 2 + j] = inputArr[N / 2 + i][M / 2 + j];
-                    if (k == 2)
-                        tmp[N / 2 + i][M / 2 + j] = inputArr[N / 2 + i][j];
-                    if (k == 3)
-                        tmp[N / 2 + i][j] = inputArr[i][j];
-                }
-            }
-        }
-    }
+void input() {
+    cin >> N >> M >> R;
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
-            inputArr[i][j] = tmp[i][j];
+            cin >> arr[i][j];
         }
     }
 }
 
-void rotateArray(int N, int M, int flag, int arr[100][100]) {
-    int tmp[100][100];
-    if (flag == 2) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M / 2; j++) {
-                int tmp = arr[i][j];
-                arr[i][j] = arr[i][M - j - 1];
-                arr[i][M - j - 1] = tmp;
-            }
-        }
-        return;
-    } else if (flag == 1) {
-        for (int i = 0; i < N / 2; i++) {
-            for (int j = 0; j < M; j++) {
-                int tmp = arr[i][j];
-                arr[i][j] = arr[N - i - 1][j];
-                arr[N - i - 1][j] = tmp;
-            }
-        }
-        return;
-    } else if (flag == 3) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                tmp[j][N - i - 1] = arr[i][j]; // 0 위치 바꾸기
-            }
-        }
-        int temp = N;
-        N = M;
-        M = temp;
-    } else if (flag == 4) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                tmp[M - j - 1][i] = arr[i][j];
-            }
-        }
-        int temp = N;
-        N = M;
-        M = temp;
-    } else if (flag == 5) {
-        for (int k = 0; k < 4; k++) {
-            for (int i = 0; i < N / 2; i++) {
-                for (int j = 0; j < M / 2; j++) {
-                    if (k == 0)
-                        tmp[i][j] = arr[N / 2 + i][j];
-                    if (k == 1)
-                        tmp[i][M / 2 + j] = arr[i][j];
-                    if (k == 2)
-                        tmp[N / 2 + i][M / 2 + j] = arr[i][M / 2 + j];
-                    if (k == 3)
-                        tmp[N / 2 + i][j] = arr[N / 2 + i][M / 2 + j];
-                }
-            }
-        }
-    } else {
-        for (int k = 0; k < 4; k++) {
-            for (int i = 0; i < N / 2; i++) {
-                for (int j = 0; j < M / 2; j++) {
-                    if (k == 0)
-                        tmp[i][j] = arr[i][M / 2 + j];
-                    if (k == 1)
-                        tmp[i][M / 2 + j] = arr[N / 2 + i][M / 2 + j];
-                    if (k == 2)
-                        tmp[N / 2 + i][M / 2 + j] = arr[N / 2 + i][j];
-                    if (k == 3)
-                        tmp[N / 2 + i][j] = arr[i][j];
-                }
-            }
-        }
-    }
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            arr[i][j] = tmp[i][j];
+void compress() {
+    int x[] = {0, (N / 2) - 1, N / 2, N - 1};
+    int y[] = {0, (M / 2) - 1, M / 2, M - 1};
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            compressMapInfo[i][j] = {x[i], y[j]};
         }
     }
 }
 
-void mappingArray(int r, int c) {
-
-    int test[100][100];
-    memset(test, 0, sizeof(test));
-    // cout << "mapping Array : " << "r : " << r << ", c : " << c << endl;
-    for (int i = r; i < N / 2 + r; i++) {
-        for (int j = c; j < M / 2 + c; j++) {
-            test[i][j] = inputArr[i][j];
+void rotation(int operate, int W, int H, bool isCompressed) {
+    if (operate == 1) {
+        for (int i = 0; i < W / 2; i++) {
+            for (int j = 0; j < H; j++) {
+                if (isCompressed)
+                    swap(compressMapInfo[i][j], compressMapInfo[W - i - 1][j]);
+                else
+                    swap(arr[i][j], arr[W - i - 1][j]);
+            }
+        }
+        return;
+    } 
+    if (operate == 2) {
+        for (int i = 0; i < W; i++) {
+            for (int j = 0; j < H / 2; j++) {
+                if (isCompressed)
+                    swap(compressMapInfo[i][j], compressMapInfo[i][H - j - 1]);
+                else
+                    swap(arr[i][j], arr[i][H - j - 1]);
+            }
+        }
+        return;
+    }
+    if (operate == 3) {
+        for (int i = 0; i < W; i++) {
+            for (int j = 0; j < H; j++) {
+                if (isCompressed)
+                    compressMapTmp[j][W - i - 1] = compressMapInfo[i][j];
+                else
+                    tmp[j][W - i - 1] = arr[i][j];
+            }
+        }
+        swap(N, M);
+        swap(W, H);
+    }
+    if (operate == 4) {
+        for (int i = 0; i < W; i++) {
+            for (int j = 0; j < H; j++) {
+                if (isCompressed)
+                    compressMapTmp[H - j - 1][i] = compressMapInfo[i][j];
+                else
+                    tmp[H - j - 1][i] = arr[i][j];
+            }
+        }
+        swap(N, M); 
+        swap(W, H);
+    }
+    if (operate == 5) {
+        int dx[] = {0, 0, W / 2, W / 2};
+        int dy[] = {0, H / 2, H / 2, 0};
+        for (int k = 0; k < 4; k++) {
+            for (int i = 0; i < W / 2; i++) {
+                for (int j = 0; j < H / 2; j++) {
+                    if (isCompressed)
+                        compressMapTmp[i + dx[k]][j + dy[k]] = compressMapInfo[i + dx[(k + 3) % 4]][j + dy[(k + 3) % 4]];
+                    else
+                        tmp[i + dx[k]][j + dy[k]] = arr[i + dx[(k + 3) % 4]][j + dy[(k + 3) % 4]];
+                }
+            }
+        }
+    }
+    if (operate == 6) {
+        int dx[] = {0, 0, W / 2, W / 2};
+        int dy[] = {0, H / 2, H / 2, 0};
+        for (int k = 0; k < 4; k++) {
+            for (int i = 0; i < W / 2; i++) {
+                for (int j = 0; j < H / 2; j++) {
+                    if (isCompressed)
+                        compressMapTmp[i + dx[k]][j + dy[k]] = compressMapInfo[i + dx[(k + 1) % 4]][j + dy[(k + 1) % 4]];
+                    else
+                        tmp[i + dx[k]][j + dy[k]] = arr[i + dx[(k + 1) % 4]][j + dy[(k + 1) % 4]];
+                }
+            }
         }
     }
 
-    cout << "test" << endl;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            cout << test[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-    if (rotation[0][0] % 4 == 0 && rotation[0][1] % 4 == 1) {
-        // cout << "DEBUG 1" << endl;
-
-        int loopCnt = 0;
-        if (rotation[0][0] / 4 == 0) loopCnt = 0;
-        else if (rotation[0][0] / 4 == 2) loopCnt = 1;
-        else if (rotation[0][0] / 4 == 3) loopCnt = 2;
-        else if (rotation[0][0] / 4 == 1) loopCnt = 3;
-        
-        for (int i = 0; i < loopCnt; i++) {
-            rotateArray(N, M, 5, test);
+    for (int i = 0; i < W; i++) {
+        for (int j = 0; j < H; j++) {
+            if (isCompressed)
+                compressMapInfo[i][j] = compressMapTmp[i][j];
+            else
+                arr[i][j] = tmp[i][j];
         }
     }
-    if (rotation[0][0] % 4 == 1 && rotation[0][1] % 4 == 0) {
-        // cout << "DEBUG 2" << endl;
-        rotateArray(N, M, 2, test);
-    }
-    if (rotation[1][1] % 4 == 0 && rotation[1][0] % 4 == 1) {
-        // cout << "DEBUG 3" << endl;
-        rotateArray(N, M, 3, test);
-        rotateArray(M, N, 3, test);
-    }
-    if (rotation[1][1] % 4 == 1 && rotation[1][0] % 4 == 0) {
-        // cout << "DEBUG 4" << endl;
-        rotateArray(N, M, 1, test);
-    }
-    if (rotation[1][1] % 4 == 0 && rotation[0][1] % 4 == 1) {
-        // cout << "DEBUG 5" << endl;
-        rotateArray(N, M, 3, test);
-        rotateArray(M, N, 1, test);
-    }
-    if (rotation[1][1] % 4 == 1 && rotation[0][1] % 4 == 0) {
-        // cout << "DEBUG 6" << endl;
-        rotateArray(N, M, 3, test);
-    }
-    if (rotation[0][0] % 4 == 0 && rotation[1][0] % 4 == 1) {
-        // cout << "DEBUG 7" << endl;
-        rotateArray(N, M, 4, test);
-        rotateArray(M, N, 1, test);
-    }
-    if (rotation[0][0] % 4 == 1 && rotation[1][0] % 4 == 0) {
-        // cout << "DEBUG 8" << endl;
-        rotateArray(N, M, 4, test);
-    }
+}
 
-    for (int i = 0; i < 100; i++) {
-        for (int j = 0; j < 100; j++) {
-            if (test[i][j])
-                result[i][j] = test[i][j];
+void mappingArray(int sX, int sY, int eX, int eY, int squareNum) {
+    int arrX[] = {0, 0, N / 2, N / 2};
+    int arrY[] = {0, M / 2, 0, M / 2};
+
+    //point (p1, p2, p3)
+    //[1] [2]
+    //[3]  4
+    pii p1 = compressMapInfo[sX][sY];
+    pii p2 = compressMapInfo[eX][sY];
+    pii p3 = compressMapInfo[sX][eY];
+
+    int p1_X = p1.first, p1_Y = p1.second;
+    int p2_X = p2.first, p2_Y = p2.second;
+    int p3_X = p3.first, p3_Y = p3.second;
+
+    //1 2
+    //3 4
+    if (p1_X < p2_X && p1_Y < p3_Y) {
+        for (int i = p1_X, X = arrX[squareNum]; i <= p2_X; i++, X++) {
+            for (int j = p1_Y, Y = arrY[squareNum]; j <= p3_Y; j++, Y++) {
+                result[X][Y] = arr[i][j];
+            }
         }
     }
-    cout << "MAPPING RESULT\n";
-    if (rotateCnt) {
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                cout << result[i][j] << " ";
+
+    //2 1
+    //4 3
+    if (p1_X > p2_X && p1_Y < p3_Y) {
+        for (int i = p1_X, X = arrX[squareNum]; i >= p2_X; i--, X++) {
+            for (int j = p1_Y, Y = arrY[squareNum]; j <= p3_Y; j++, Y++) {
+                result[X][Y] = arr[i][j];
+            }
+        }
+    }
+
+    //4 3
+    //2 1
+    if (p1_X > p2_X && p1_Y > p3_Y) {
+        for (int i = p1_X, X = arrX[squareNum]; i >= p2_X; i--, X++) {
+            for (int j = p1_Y, Y = arrY[squareNum]; j >= p3_Y; j--, Y++) {
+                result[X][Y] = arr[i][j];
+            }
+        }
+    }
+
+    //3 4
+    //1 2
+    if (p1_X < p2_X && p1_Y > p3_Y) {
+        for (int i = p1_X, X = arrX[squareNum]; i <= p2_X; i++, X++) {
+            for (int j = p1_Y, Y = arrY[squareNum]; j >= p3_Y; j--, Y++) {
+                result[X][Y] = arr[i][j];
+            }
+        }
+    }
+
+    //ROTATION, (FOR(Y, X) -> [j][i])
+    //4 2
+    //3 1
+    if (p1_X > p3_X && p1_Y > p2_Y) {
+        for (int i = p1_Y, X = arrX[squareNum]; i >= p2_Y; i--, X++) {
+            for (int j = p1_X, Y = arrY[squareNum]; j >= p3_X; j--, Y++) {
+                result[X][Y] = arr[j][i];
+            }
+        }
+    }
+
+    //3 1
+    //4 2
+    if (p1_X > p3_X && p1_Y < p2_Y) {
+        for (int i = p1_Y, X = arrX[squareNum]; i <= p2_Y; i++, X++) {
+            for (int j = p1_X, Y = arrY[squareNum]; j >= p3_X; j--, Y++) {
+                result[X][Y] = arr[j][i];
+            }
+        }
+    }
+
+    //1 3
+    //2 4
+    if (p1_X < p3_X && p1_Y < p2_Y) {
+        for (int i = p1_Y, X = arrX[squareNum]; i <= p2_Y; i++, X++) {
+            for (int j = p1_X, Y = arrY[squareNum]; j <= p3_X; j++, Y++) {
+                result[X][Y] = arr[j][i];
+            }
+        }
+    }
+
+    //2 4
+    //1 3
+    if (p1_X < p3_X && p1_Y > p2_Y) {
+        for (int i = p1_Y, X = arrX[squareNum]; i >= p2_Y; i--, X++) {
+            for (int j = p1_X, Y = arrY[squareNum]; j <= p3_X; j++, Y++) {
+                result[X][Y] = arr[j][i];
+            }
+        }
+    }
+}
+
+void print(bool isCompressed) {
+    if (!isCompressed) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                cout << arr[i][j] << " ";
             }
             cout << endl;
         }
     } else {
+        mappingArray(0, 0, 1, 1, 0); //top-left square
+        mappingArray(0, 2, 1, 3, 1); //top-right square
+        mappingArray(2, 0, 3, 1, 2); //bottom-left square
+        mappingArray(2, 2, 3, 3, 3); //bottom-right square
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 cout << result[i][j] << " ";
@@ -259,77 +240,20 @@ void mappingArray(int r, int c) {
         }
     }
 }
-
-
-
 int main() {
-    std::cin >> N >> M >> R;
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            std::cin >> inputArr[i][j];
-        }
-    }
-
-    int operate;
-    for (int i = 0; i < R; i++) {
-        std::cin >> operate;
-        operCnt[operate]++;
-    }
-
-    operCnt[1] %= 2;
-    operCnt[2] %= 2;
-    operCnt[3] %= 4;
-    operCnt[4] %= 4;
-    operCnt[5] %= 4;
-    operCnt[6] %= 4;
-
-    rotateCnt = abs(operCnt[3] - operCnt[4]) % 2;
-
-    if (N >= 4 && M >= 4) {
-        for (int i = 1; i <= 6; i++) {
-            while (operCnt[i]--) {
-                rotateArray(4, 4, i, rotation);
-            }
-        }
-
-        mappingArray(0, 0);
-        mappingArray(N / 2, 0);
-        mappingArray(0, M / 2);
-        mappingArray(N / 2, M / 2);
-    } else {
-        for (int i = 1; i <= 6; i++) {
-            while (operCnt[i]--) {
-                rotateArray2(i);
-            }
-        }
-    }
-
+    fastio;
     
-    // for (int i = 0; i < 4; i++) {
-    //     for (int j = 0; j < 4; j++) {
-    //         std::cout << rotation[i][j] << " ";
-    //     }
-    //     std::cout << endl;
-    // }
+    input();
+    compress();
 
-
-
-
-    if (rotateCnt) {
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                cout << result[i][j] << " ";
-            }
-            cout << endl;
-        }
-    } else {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                cout << result[i][j] << " ";
-            }
-            cout << endl;
-        }
+    bool isCompressed = (N >= 4) && (M >= 4);
+    while (R--) {
+        int operate; cin >> operate;
+        if (isCompressed) 
+            rotation(operate, 4, 4, isCompressed);
+        else
+            rotation(operate, N, M, isCompressed);
     }
+    print(isCompressed);
     return 0;
 }
