f66f135d97127617f488250d1f89bced9e3c558f
chlee <chlee@student.42seoul.kr>
2024-01-16 18:13:41 +0900

    ADD: solved B5 implementation problem

---
+++ Baekjoon/24883.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define fastio cin.tie(0)->sync_with_stdio(0)
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+int main() {
+    fastio;
+    char N; cin >> N;
+    if (N == 'N' || N == 'n') cout << "Naver D2" << endl;
+    else cout << "Naver Whale" << endl;
+    return 0;
+}
