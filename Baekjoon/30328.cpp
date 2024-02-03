b8b14f54861e59b111bfa5643fb900db8b92c2a8
chlee <chlee@student.42seoul.kr>
2024-02-03 12:39:17 +0900

    ADD: solved B5 implementation problem

---
+++ Baekjoon/30328.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define fastio cin.tie(0)->sync_with_stdio(0)
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+int main() {
+    fastio;
+    int N; cin >> N;
+    cout << N * 4000 << endl;
+    return 0;
+}
