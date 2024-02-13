15f81b09aa5eef5aa31b187e70ab63ddc35db90b
chlee <chlee@student.42seoul.kr>
2024-02-13 19:01:13 +0900

    ADD: Contest 231231_GoodByeBOJ

---
+++ Tutorial/Geometry/Geometry.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define fastio cin.tie(0)->sync_with_stdio(0)
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+struct Point {
+    ll x, y;
+    Point() {};
+    Point(ll x, ll y) : x(x), y(y) {};
+    bool operator< (const Point& p) const {
+        if (y == p.y) return x < p.x;
+        return y < p.y;
+    }
+};
+
+int N;
+vector<Point> p;
+
+ll ccw(Point p1, Point p2, Point p3) {
+    ll ret = (p1.x * p2.y + p2.x * p3.y + p3.x * p1.y) - (p1.y * p2.x + p2.y * p3.x + p3.y * p1.x);
+    return (ret > 0) - (ret < 0);
+}
+
+ll cross(Point &p1, Point &p2) {
+    return p1.x * p2.y - p1.y * p2.x;
+}
+
+ll dist(Point &p1, Point &p2) {
+    return (p1.x - p2.x) * (p1.x - p2.x) +
+           (p1.y - p2.y) * (p1.y - p2.y);
+}
+
+bool cmp(Point &p1, Point &p2) {
+    ll ret = ccw(p[0], p1, p2);
+    if (ret == 0) return dist(p[0], p1) < dist(p[0], p2);
+    return ret > 0;
+}
+
+
+int main() {
+    fastio;
+    
+    return 0;
+}
