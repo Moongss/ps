ae5a55cd3f12b3b23a3bd5fbd0a7244e3e1c52f7
chlee <chlee@student.42seoul.kr>
2024-02-13 18:57:08 +0900

    WIP

---
+++ Baekjoon/16765.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define fastio cin.tie(0)->sync_with_stdio(0)
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+int N, K;
+vector<int> A;
+int dp[1001][1001];
+
+
+int main() {
+    fastio;
+    
+    return 0;
+}
