311698aba25fbb2d1b6367f450b6a4c7e55b5bcd
chlee <chlee@student.42seoul.kr>
2024-02-13 18:59:23 +0900

    ADD: solved B4 implementation problem

---
+++ Baekjoon/3046.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define fastio cin.tie(0)->sync_with_stdio(0)
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+int main() {
+    fastio;
+    int a, b; cin >> a >> b; cout << b * 2 - a << endl;
+    return 0;
+}
