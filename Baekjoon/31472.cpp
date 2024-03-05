a1ddc29ab30f35f670b7e6e8049403d77f8b92ed
chlee <chlee@student.42seoul.kr>
2024-03-05 09:44:06 +0900

    ADD: solved B5 implementation problem

---
+++ Baekjoon/31472.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define fastio cin.tie(0)->sync_with_stdio(0)
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+int main() {
+    fastio;
+    
+    int N; cin >> N;
+    cout << sqrt((N * 2)) * 4 << endl;
+    return 0;
+}
