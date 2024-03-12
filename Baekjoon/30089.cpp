15cd51f23af224e3f5e95e88c92da6577e58b143
chlee <chlee@student.42seoul.kr>
2024-03-12 10:54:51 +0900

    WIP

---
+++ Baekjoon/30089.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define fastio cin.tie(0)->sync_with_stdio(0)
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+int main() {
+    fastio;
+    
+    int T; cin >> T;
+    while (T--) {
+        string str; cin >> str;
+        
+    }
+    return 0;
+}
